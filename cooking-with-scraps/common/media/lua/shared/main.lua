98be4b8bef0403144d6b398a25c2e1f3c120b7a7
larskue <lars@kuehmichel.de>
2025-09-15 11:06:09 +0200

    Initial working setup

---
+++ cooking-with-scraps/common/media/lua/shared/main.lua
@@ -0,0 +1,29 @@
+
+local function getField(instance, name)
+    local offset = string.len(name)
+    for i = 0, getNumClassFields(instance) - 1 do
+        local field = getClassField(instance, i)
+        if string.sub(tostring(field), -offset) == name then
+            return field
+        end
+    end
+    return nil
+end
+
+local function main()
+    print("===== Cooking With Scraps =====")
+    local recipes = getEvolvedRecipes()
+
+    for i = 0, recipes:size() - 1 do
+        local recipe = recipes:get(i)
+        local field = getField(recipe, "maxItems")
+        if field then
+            field:setAccessible(true)
+            field:setInt(recipe, 9999)
+        end
+    end
+
+    print("===============================")
+end
+
+main()
